c19f753aec305fa5d14511ddcbc76d1067ea2afa
raphadeluca <raphadeluca@bluewin.ch>
2019-04-23 21:52:00 +0200

    Link your quiz

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/b6e54a57-aa24-436f-be39-f075b842f3bb/deploy-status)](https://app.netlify.com/sites/inspiring-kare-1888f6/deploys)
 
 # :grapes: gatsby-starter-quiz
-Create rich quizzes with Gatsby & Mdx.
+Create rich quizzes with Gatsby & Mdx. No need of database or headless CMS. Manage your data directly in your Mdx file's frontmatter and write your content in the body. Customize your HTML tags, use other's react components or write your owns. Navigation will be automatically created between each question.
 
 ➡️ **[See a live example](https://gatsby-starter-quiz.netlify.com//)**
 
@@ -23,7 +23,6 @@ $ yarn
 
 ## Usage
 
-With the gatsby-starter-quiz you can create
 ### :one: Files
 Edit your quizzes inside the `/quizzes` folder:
 - create a folder for each quiz
@@ -66,9 +65,22 @@ answers: [
 Look at the examples in the quizzes folder to see how you can use React within a Markdown file (That's MDX!).
 For a full explanation, read the MDX docs and the gatsby-MDX implementation.
 
-### :four: Link it from home page
--
+### :four: Link it from the home page
 
+In the `/pages/index.js` file create a link to your quiz with the Card component.
 
-### Configuration
+```js
+  <Card
+    backgroundColor='#5bc0eb'
+    title="CSS"
+    description="Take a little quiz to test your CSS knowledges..."
+    link="css-01"
+  />
+```
+
+### :five: Deploy
+- create a repository on github with your project
+- deploy it on Netlify
+
+## :art: Configuration
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,37 +9,37 @@ import { media } from '../tokens'
 export default function Index () {
   return (
     <Layout>
-      <div style= {{height: '60px'}}>
-      <h1>Ultimate Quizzes</h1>
+      <div style={{ height: '60px' }}>
+        <h1>Ultimate Quizzes</h1>
       </div>
       <div
-          css={css`
+        css={css`
             display: grid;
             grid-template-columns: 1fr;
             grid-gap: 20px;
-            @media ${media.medium} {
+            @media ${ media.medium } {
               grid-template-columns: repeat(auto-fit, 300px);
             }
           `}
->
-            <Card
-            backgroundColor = '#5bc0eb'
-            title = "CSS"
-            description = "Take a little quiz to test your CSS knowledges..."
-            link = "css-01"
-            />
-             <Card
-            backgroundColor = '#fa7921'
-            title = "MDX"
-            description = "Take a little quiz to test your MDX knowledges..."
-            link = "mdx-01"
-            />
-             <Card
-            backgroundColor = '#9bc53d'
-            title = "GATSBY"
-            description = "Take a little quiz to test your Gatsby knowledges..."
-            link = "gatsby-01"
-            />
+      >
+        <Card
+          backgroundColor='#5bc0eb'
+          title="CSS"
+          description="Take a little quiz to test your CSS knowledges..."
+          link="css-01"
+        />
+        <Card
+          backgroundColor='#fa7921'
+          title="MDX"
+          description="Take a little quiz to test your MDX knowledges..."
+          link="mdx-01"
+        />
+        <Card
+          backgroundColor='#9bc53d'
+          title="GATSBY"
+          description="Take a little quiz to test your Gatsby knowledges..."
+          link="gatsby-01"
+        />
       </div>
     </Layout >
   )
